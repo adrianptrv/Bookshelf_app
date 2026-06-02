43b6b5ed9315c874ff22f2ceda341f61cb5b5dfe
Adrian Petrov <adrian.ptrv1@gmail.com>
2026-06-03 01:50:14 +0300

    feat(): remove dark styles + other visual fixes

--- script.ts
+++ script.ts
@@ -25,8 +25,8 @@ class Book {
     add() {
         const list = document.getElementById("book-list") as HTMLTableSectionElement;
         const newBook = document.createElement('tr');
-        newBook.classList.add("odd:bg-white", "odd:dark:bg-gray-900", "even:bg-gray-50", "even:dark:bg-gray-800", "border-b", "dark:border-gray-700");
-        newBook.innerHTML = '<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">' + this.name + '</th><td class="px-6 py-4">' + this.author + '</td><td class="px-6 py-4">' + this.category + '</td><td class="px-6 py-4">' + this.year + '</td><td class="px-6 py-4">' + this.read + '</td><td class="px-6 py-4">' + this.isbn + '</td>' + '<td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer" onclick="delBook(this)">Remove</a></td>';
+        newBook.classList.add("odd:bg-white", "even:bg-gray-50", "border-b");
+        newBook.innerHTML = '<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">' + this.name + '</th><td class="px-6 py-4">' + this.author + '</td><td class="px-6 py-4">' + this.category + '</td><td class="px-6 py-4">' + this.year + '</td><td class="px-6 py-4">' + this.read + '</td><td class="px-6 py-4">' + this.isbn + '</td>' + '<td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 hover:underline cursor-pointer" onclick="delBook(this)">Remove</a></td>';
         list.appendChild(newBook);
     }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  darkMode: false,
+content: ["./**/*.html", "./**/*.js"],
   theme: {
     extend: {},
   },
